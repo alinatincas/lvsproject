204967f43460f2856a8ceba99fe92c88b8285bf4
Crone <crone1992@gmail.com>
2018-04-03 13:14:36 +0200

    Navbar almost done. -Navbar needs alignment. -Added feature to highlight currentpage in nav -Minor changes to master -Added vaesteder file for test

--- LVS/assets/lvs.js
+++ LVS/assets/lvs.js
@@ -1,5 +1,8 @@
 ﻿$(document).ready(function () {
     // get current URL path and assign 'active' class
     var pathname = window.location.pathname;
-    $('.nav > li > a[href="' + pathname + '"]').parent().addClass('active');
+    console.log(pathname);
+
+    $('nav > ul > li> a[href="' + pathname + '"]').parent().addClass('active_nav');
+    //$("#\\/").addClass('active_nav');
 })
