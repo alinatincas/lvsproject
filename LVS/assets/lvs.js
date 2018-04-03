a12eb3bab40b86aaa616c66ff238da4c74878dde
Crone <crone1992@gmail.com>
2018-04-03 14:34:13 +0200

    Minor changes to navbar. Still needs mores work with allignment.

--- LVS/assets/lvs.js
+++ LVS/assets/lvs.js
@@ -1,8 +1,8 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function() {
     // get current URL path and assign 'active' class
     var pathname = window.location.pathname;
     console.log(pathname);
 
     $('nav > ul > li> a[href="' + pathname + '"]').parent().addClass('active_nav');
     //$("#\\/").addClass('active_nav');
-})
+});
