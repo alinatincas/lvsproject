d98d57ecf7b42d8cbfe8e8aabae571edb188f73e
Jesper Tastesen <jesper@tastesen.dk>
2018-04-04 12:59:12 +0200

    new gulp file with minify

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,27 @@ var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
 
 gulp.task('scss', function () {
     return gulp.src('LVS/assets/scss/style.scss')
         .pipe(sass())
         .pipe(autoprefixer('last 2 version', 'safari 6', 'ie 10', 'ie 11'))
         .pipe(gulp.dest('LVS/assets/css'))
+    gulp.task('minify');
 });
 
-gulp.task('minify', function () {
-    gulp.src('LVS/assets/css/**/*.css')
+gulp.task('minify', ['scss'], function () {
+    del(['LVS/assets/css/*.css', '!LVS/assets/css/style.css']).then(paths => {
+        console.log('Deleted files and folders:\n', paths.join('\n'));
+    });
+    gulp.src('LVS/assets/css/**/style.css')
         .pipe(minifyCSS())
         .pipe(autoprefixer('last 2 version', 'safari 6', 'ie 10', 'ie 11'))
+        .pipe(sourcemaps.init())
         .pipe(concat('style.min.css'))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('LVS/assets/css'))
 });
 
