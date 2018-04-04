8a29dce488d7296949ffaacd8d17449ad584b4da
Jesper Tastesen <jesper@tastesen.dk>
2018-04-04 11:15:55 +0200

    added Gulp scss compile command
    do: 
    npm install
    gulp scss // compiles scss files
    gulp watch // automatic watches for changes on any scss file

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+gulp.task('scss', function () {
+    return gulp.src('LVS/assets/scss/style.scss')
+        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(gulp.dest('LVS/assets/css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('LVS/assets/scss/**/*.scss', ['sass']);
+    // Other watchers
+})
