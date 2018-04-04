6a3e376b92fbf05ad74a99958791ee8f35c91eed
Jesper Tastesen <jesper@tastesen.dk>
2018-04-04 12:23:14 +0200

    update gulp tasks and readme

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,31 @@ var useref = require('gulp-useref');
 var cssnano = require('gulp-cssnano');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
 
 gulp.task('scss', function () {
     return gulp.src('LVS/assets/scss/style.scss')
-        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(sass())
         .pipe(gulp.dest('LVS/assets/css'))
 });
 
+gulp.task('minify', function () {
+    gulp.src('LVS/assets/css/**/*.css')
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 version', 'safari 6', 'ie 10', 'ie 11'))
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('LVS/assets/css'))
+});
+
+
 gulp.task('watch', function () {
     gulp.watch('LVS/assets/scss/**/*.scss', ['scss']);
+    gulp.watch('LVS/assets/scss/**/*.scss', ['minify']);
     // Other watchers
 })
+
+gulp.task('default', ['scss', 'minify', 'watch']);
