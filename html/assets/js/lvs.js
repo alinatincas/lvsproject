646d2a541e64c96f7a01546f52337a1025945916
Jesper Tastesen <jesper@tastesen.dk>
2018-04-06 08:09:38 +0200

    fontawesomefix and other minor ajustments

--- html/assets/js/lvs.js
+++ html/assets/js/lvs.js
@@ -1,48 +1,48 @@
 ﻿$(document).ready(function () {
     // get current URL path and assign 'active' class
-    var pathname = window.location.pathname;
-    console.log(pathname);
+    //var pathname = window.location.pathname;
+    //console.log(pathname);
 
-    $('nav > ul > li> a[href="' + pathname + '"]').parent().addClass('active_nav');
+    //$('nav > ul > li> a[href="' + pathname + '"]').parent().addClass('active_nav');
     //$("#\\/").addClass('active_nav');
 
     $("div[data-includeHTML]").each(function () {
         $(this).load($(this).attr("data-includeHTML"));
     });
 
-    squareboxes();
+    // squareboxes();
 });
 
-function squareboxes() {
+// function squareboxes() {
 
-    //squarebox
+//     //squarebox
 
-    $('.squarebox').each(function (index) {
+//     $('.squarebox').each(function (index) {
 
-        // for at sikre at alle elementer med classen .squarebox benyttes 'this'.
-        // 'this' betyder at det er det nuvÃ¦rende element i loopet det bliver hentet width pÃ¥.
+//         // for at sikre at alle elementer med classen .squarebox benyttes 'this'.
+//         // 'this' betyder at det er det nuvÃ¦rende element i loopet det bliver hentet width pÃ¥.
 
-        var cwWidth = $(this).width();
+//         var cwWidth = $(this).width();
 
-        $(this).css({
+//         $(this).css({
 
-            'height': cwWidth + 'px'
+//             'height': cwWidth + 'px'
 
-        });
-    });
+//         });
+//     });
 
-    $('.squarebox-halfheight').each(function (index) {
+//     $('.squarebox-halfheight').each(function (index) {
 
-        // for at sikre at alle elementer med classen .squarebox benyttes 'this'.
-        // 'this' betyder at det er det nuvÃ¦rende element i loopet det bliver hentet width pÃ¥.
+//         // for at sikre at alle elementer med classen .squarebox benyttes 'this'.
+//         // 'this' betyder at det er det nuvÃ¦rende element i loopet det bliver hentet width pÃ¥.
 
-        var cwWidth = $(this).width();
-        var cwWidthHalf = cwWidth / 2;
+//         var cwWidth = $(this).width();
+//         var cwWidthHalf = cwWidth / 2;
 
-        $(this).css({
+//         $(this).css({
 
-            'height': cwWidthHalf + 'px'
+//             'height': cwWidthHalf + 'px'
 
-        });
-    });
-}
+//         });
+//     });
+// }
