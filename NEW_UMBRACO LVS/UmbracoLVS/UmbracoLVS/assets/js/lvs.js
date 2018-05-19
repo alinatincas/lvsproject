7c4f8971a08d2bd4ef29111005fcd8a3262ecf66
Jesper Tastesen <jesper@tastesen.dk>
2018-05-19 21:47:38 +0200

    news page+ facebook

--- NEW_UMBRACO LVS/UmbracoLVS/UmbracoLVS/assets/js/lvs.js
+++ NEW_UMBRACO LVS/UmbracoLVS/UmbracoLVS/assets/js/lvs.js
@@ -1,58 +1,64 @@
 ﻿$(document).ready(function () {
     //Below code is used on the search result list, to change active link
-    $('.list-group a').click(function (e) {
-        e.preventDefault()
-        $('.list-group').find('a').removeClass('active').addClass('text-dark');
-        $(this).removeClass('text-dark').addClass('active');
-        $("#Centertxt").html('Test');
-        $.ajax({
-            url: "~/Shelter", success: function (result) {
-                $("#Centertxt").html(result);
-            }
-        });
-    });
-    $()
+
+    //$('.list-group a').click(function (e) {
+    //    e.preventDefault()
+    //    $('.list-group').find('a').removeClass('active').addClass('text-dark');
+    //    $(this).removeClass('text-dark').addClass('active');
+    //    $("#Centertxt").html('Test');
+    //    $.ajax({
+    //        url: "~/Shelter", success: function (result) {
+    //            $("#Centertxt").html(result);
+    //        }
+    //    });
+    //}); 
+
+    // searchbox activator
     $("#search-icon").click(function (e) {
+
         if ($("#searchbar").hasClass("show")) {
+
             $("#searchbar").removeClass("show");
+
         } else {
+
             $("#searchbar").addClass("show");
         }
     });
 
-    // squareboxes();
+    squareboxes();
 });
 
-// function squareboxes() {
+ function squareboxes() {
 
-//     //squarebox
+     //squarebox
 
-//     $('.squarebox').each(function (index) {
+     $('.squarebox').each(function (index) {
 
-//         // for at sikre at alle elementer med classen .squarebox benyttes 'this'.
-//         // 'this' betyder at det er det nuvÃ¦rende element i loopet det bliver hentet width pÃ¥.
+         // for at sikre at alle elementer med classen .squarebox benyttes 'this'.
+         // 'this' betyder at det er det nuvÃ¦rende element i loopet det bliver hentet width pÃ¥.
 
-//         var cwWidth = $(this).width();
+         var cwWidth = $(this).width();
 
-//         $(this).css({
+         $(this).css({
 
-//             'height': cwWidth + 'px'
+             'height': cwWidth + 'px'
 
-//         });
-//     });
+         });
+     });
 
-//     $('.squarebox-halfheight').each(function (index) {
+     $('.squarebox-halfheight').each(function (index) {
 
-//         // for at sikre at alle elementer med classen .squarebox benyttes 'this'.
-//         // 'this' betyder at det er det nuvÃ¦rende element i loopet det bliver hentet width pÃ¥.
+         // for at sikre at alle elementer med classen .squarebox benyttes 'this'.
+         // 'this' betyder at det er det nuvÃ¦rende element i loopet det bliver hentet width pÃ¥.
 
-//         var cwWidth = $(this).width();
-//         var cwWidthHalf = cwWidth / 2;
+         var cwWidth = $(this).width();
+         var cwWidthHalf = cwWidth / 2;
 
-//         $(this).css({
+         $(this).css({
 
-//             'height': cwWidthHalf + 'px'
+             'height': cwWidthHalf + 'px'
 
-//         });
-//     });
-// }
+         });
+     });
+ }
