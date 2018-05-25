d397be6ed38d0aebeb6b9db158fa448f1905f9cf
Alina Tincas <alin0831@edu.eal.dk>
2018-05-25 12:59:55 +0200

    Added real content to the væresteder result

--- NEW_UMBRACO LVS/UmbracoLVS/UmbracoLVS/App_Data/Models/Item.generated.cs
+++ NEW_UMBRACO LVS/UmbracoLVS/UmbracoLVS/App_Data/Models/Item.generated.cs
@@ -21,7 +21,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 namespace Umbraco.Web.PublishedContentModels
 {
 	// Mixin content Type 1157 with alias "item"
-	/// <summary>item</summary>
+	/// <summary>Details</summary>
 	public partial interface IItem : IPublishedContent
 	{
 		/// <summary>Description</summary>
@@ -31,7 +31,7 @@ namespace Umbraco.Web.PublishedContentModels
 		IPublishedContent Image { get; }
 	}
 
-	/// <summary>item</summary>
+	/// <summary>Details</summary>
 	[PublishedContentModel("item")]
 	public partial class Item : PublishedContentModel, IItem
 	{
